b94c6c7d3e45d59acd5f94b18fec5b51bbded945
Maria <m.tur67@gmail.com>
2022-12-08 03:12:21 +0300

    modified file homework4

--- seminar4/homework4.py
+++ seminar4/homework4.py
@@ -1,15 +1,31 @@
-import os
-import random
+from math import pi
+from os import chdir
+from random import randint as rnd
 import sympy
+
 
 # 1.Вычислить число Пи c заданной точностью d
 # Пример:
 # при d = 0.001, π = 3.141
 # при d = 0.0001, π = 3.1415
+# print(pi)
+# # Вариант 1
+# d = 0.001
+# d = str(d)
+# d = d[2:len(d)]
+# d = len(d)
+# print(round(pi, d))
+
+# # Вариант с семинара
+# def format(pi:float, d:float) -> float:
+#     d = str(d)
+#     d = len(d[d.find('.')+1::])
+#     return float(f'{pi:0.{d}f}')
+# d = 0.001
+# print(format(pi, d))
 
 
-
-# 2 и 3 задачи обединила
+# 2 и 3 задачи обьединила
 # 2.Задайте натуральное число N. Напишите программу, 
 # которая составит список простых множителей числа N.
 # 6 -> [2,3]
@@ -31,7 +47,6 @@ import sympy
 # print(f'Список из неповторяющихся элементов: {res}')
 
 
-
 # 4. Задана натуральная степень k. 
 # Сформировать случайным образом список коэффициентов (значения от 0 до 100) многочлена 
 # и записать в файл многочлен степени k. (записать в строку)
@@ -39,13 +54,21 @@ import sympy
 # k=2 => 2*x^2 + 4*x + 5 или x^2 + 5 или 10*x**2
 # k=3 => 2*x^3 + 4*x^2 + 4*x + 5
 
-# def write_file(str):
-#     os.chdir(r'C:\Users\Мария\Desktop\Учеба\2 четверть\1. Знакомство с Python\Семинары и домашка\Python\seminar4')
-#     with open('file.txt', 'w') as f:
-#         f.write(str)
+def write_file(stroka):
+    chdir(r'C:\Users\Мария\Desktop\Учеба\2 четверть\1. Знакомство с Python\Семинары и домашка\Python\seminar4')
+    with open('file.txt', 'w') as f:
+        f.write(stroka)
 
-# k = 2 
+k = 2
+stroka = ''
+for i in range(k, -1, -1):
+    stroka += f'{rnd(0,2)}*x**{i}+'
+    if i == 0:
+        stroka += f'{rnd(0,100)}*x**{i}'
 
+print(stroka)
+write_file(stroka)
+    
 
 
 # 5.Даны два файла, в каждом из которых находится запись многочлена. 
@@ -53,28 +76,20 @@ import sympy
 # В file1.txt: 2*x**2 + 4*x + 5
 # В file2.txt: 4*x**2 + 1*x + 4
 # Результирующий файл: 6*x**2 + 5*x + 9
+# chdir(r'C:\Users\Мария\Desktop\Учеба\2 четверть\1. Знакомство с Python\Семинары и домашка\Python\seminar4')
+# with open('file1.txt') as f:
+#     a = f.read()
+# print(a)
 
-os.chdir(r'C:\Users\Мария\Desktop\Учеба\2 четверть\1. Знакомство с Python\Семинары и домашка\Python\seminar4')
+# with open('file2.txt') as f:
+#     b = f.read()
+# print(b)
 
-with open('file1.txt', 'r') as f:
-    a = f.read()
-print(a)
-print(type(a))
-
-with open('file2.txt', 'r') as f:
-    b = f.read()
-print(b)
-print(type(b))
-
-x = sympy.Symbol('x')
-c = sympy.simplify(a+b)
-print(c)
-print(type(c))
+# x = sympy.Symbol('x')
+# c = sympy.simplify(a + '+' + b)
+# c = str(c)
+# print(c)
 
 # with open('res.txt', 'w') as f:
-#     f.write()
-
-
-# a = 2*x**2 + 4*x + 5
-# b = 4*x**2 + 1*x + 4
+#     f.write(c)
 
