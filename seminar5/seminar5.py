24b7ad08c9337928e4d3696162e746db2d50ada0
Maria <m.tur67@gmail.com>
2022-12-17 00:12:52 +0300

    modified file seminar5

--- seminar5/seminar5.py
+++ seminar5/seminar5.py
@@ -6,23 +6,33 @@
 # 1 2 3 4 6 7 -> 5
 # 1 3 -> 2
 # 3 4 6 7 8 -> 5
-# data = list(map(int, input().split()))
-
-
+stroka = '1 2 3 4 6 7'
+data = list(map(int, stroka.split()))
+print(data)
+li = []
+for i in range(1, len(data)):
+    if data[i]-1 != data[i-1]: 
+        li.append(data[i]-1)
+print(li)
 
 
 # Дан список чисел. Создайте список, в который попадают числа, описываемые возрастающую последовательность.
 # Порядок элементов менять нельзя.
 # Пример: 
 # [1, 5, 2, 3, 4, 6, 1, 7] => [1, 2, 3] или [1, 7] или [1, 6, 7] и т.д.
-data = [1, 5, 2, 3, 4, 6, 1, 7]
-li = []
-
-for i in range(len(data)):
-    if data[i] < data[i+1]:    # хn < хn+1
-        li.append(i)
-print(li)
-
+# data = [1, 5, 2, 3, 4, 6, 1, 7]
+# Вариант 1
+# a = [data[0]]
+# for i in data:
+#     if i > max(a):   
+#        a.append(i)
+# print(a)
+# Вариант 2
+# li = []
+# for i in range(len(data)):
+#     if data[i] == max(data[:i+1:]) and data[i] not in li:
+#             li.append(data[i])
+# print(li)
 
 # Напишите программу, удаляющую из текста все слова, содержащие "абв".
 # абвгд гдежз жзе абв ыопыпт' -> ' гдежз жзе ыопыпт'
