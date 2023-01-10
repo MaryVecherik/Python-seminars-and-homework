994453df4ab8947676c9c8b37911bb6dcca2ca83
Maria <m.tur67@gmail.com>
2023-01-10 22:11:40 +0300

    modified homework9

--- seminar9/homework/bot.py
+++ seminar9/homework/bot.py
@@ -18,9 +18,17 @@ def send_welcome(message):
     
     while True:
             if move[message.chat.id] % 2 == 0:
-                candys[message.chat.id] = move_people(player, candys[message.chat.id], max_take)
+                bot.register_next_step_handler(bot.send_message(message.chat.id, f'{player}, Ваш ход... '), move_people(message.text))
+                # candys[message.chat.id] = move_people(player, candys[message.chat.id], max_take)
             else:
-                candys[message.chat.id] = move_bot(candys[message.chat.id], max_take)
+                move = candys[message.chat.id] % (max_take + 1)
+                if move == 0:
+                    move = rnd(1, max_take) if candys[message.chat.id] >= max_take else candys[message.chat.id]
+                    bot.send_message(message.chat.id, f'Бот забрал {move} конфет')
+                    candys[message.chat.id] -= move
+                    bot.send_message(message.chat.id, f'Осталось {candys[message.chat.id]} конфет')
+                # candys[message.chat.id] = move_bot(candys[message.chat.id], max_take)
+
 
             if move[message.chat.id] >= (candys[message.chat.id] // max_take) - 1:
                 temp = win(candys[message.chat.id], move[message.chat.id], 'Бот')
@@ -30,12 +38,11 @@ def send_welcome(message):
             move[message.chat.id] += 1
 
 
-
 def move_people(message):
     """Ход человека"""
     global player, max_take
     while True:
-        bot.send_message(message.chat.id, f'{player}, Ваш ход... ')
+        # bot.send_message(message.chat.id, f'{player}, Ваш ход... ')
         move = int(message.text)
         if move > 0 and move <= max_take and move <= candys[message.chat.id]:
             bot.send_message(message.chat.id, f'Ты забрал(а) {move} конфет')
@@ -44,7 +51,8 @@ def move_people(message):
             break
         else:
             bot.send_message(message.chat.id, f'Столько взять нельзя. Можно взять до {max_take} или не больше оставшегося количества конфет')
-        return candys[message.chat.id]
+        return 
+        # candys[message.chat.id]
 
 
 def move_bot(message):
